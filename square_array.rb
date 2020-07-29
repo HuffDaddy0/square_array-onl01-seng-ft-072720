29882050dc5a5fa056184ba6e48255e5d2a7096a
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 13:18:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-#require 'pry'
+require 'pry'
 def square_array(array)
   new_arr = []
   array.each do |element|
     new_element = element ** 2
     new_arr.push(new_element)
-#binding.pry
+binding.pry
   end
 end
